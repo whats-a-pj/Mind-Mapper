19ec4442cc9227262cf324e554e3ec5027cdd139
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-09-24 14:13:12 -0600

    Updated project & user models

--- models/Project.js
+++ models/Project.js
@@ -1,9 +1,66 @@
-//const { Model, DataTypes } = require('sequelize');
-//const sequelize = require('../config/connection');
+//we can use this model specifically for the actual mind map itself??
+const { /* UUIDV4, */ Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-//class Project extends Model {}
+class Project extends Model {}
 
-//Project.init
-//building project model here
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //user story question
+        acceptance_criteria: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //packages question
+        pkg_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //notes question
+        note: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //code snippet question
+        snippet: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //wireframe question- p much user can just input their link here but idk if link is a datatype lol
+        wireframe_link: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //resources question
+        resource_name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+    },
+);
 
-//module.exports = Project;
+module.exports = Project;
