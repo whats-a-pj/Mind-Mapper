4acc3caa49a5c59b05b8476f492615084d9e8076
saevans86 <sam_evans1986@hotmail.com>
2023-09-28 21:45:43 -0600

    updated connection.js file

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening- check http://localhost:${PORT}`));
+  app.listen(PORT, "0.0.0.0", () => console.log(`Now listening- check http://localhost:${PORT}`));
 });
