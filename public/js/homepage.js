155b47c60466f69231a9d046d40199db55b16d58
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-10-02 11:29:34 -0600

    Got eventlisteners working for sidebar toggle and added fetch, changed ids and classes

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,98 +1,121 @@
-const dashboardRo = async (event) => {
-    event.preventDefault()
 
-    const POSTMindMap = document.querySelector('#mind-map') // click into single MM *update based on handlebars
-    const editMindMap = document.querySelector('#editMm') // edit display on the dashboard
-    const newMindMap = document.querySelector('#newMM') // NEW MM
-    const deleteMindMap = document.querySelector('#deleteMM') // delete
-    const mmlist = document.querySelectorAll('li') 
-    const title = document.querySelector('title')
+/***********************************************/
+//todo SIDEBAR eventlistener
+console.log("hello world")
+// console.log(sidebar)
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('sidebar');
+    const sidebarContent = document.getElementById('sidebar-content');
+
+sidebar.addEventListener('click', function() {
+    console.log('test')
+    sidebarContent.classList.toggle('sidebar');
+});
+});
+//todo NEW PROJECT eventlistener
+
+//todo SAVED PROJECTS eventlisteners??
+
+/*********************************************/
+//todo eventlisteners for circles 1, 2, 3.
 
 
-    // function sidebarToggler() {
+/*********************************************/
+// const dashboardRo = async (event) => {
+//     event.preventDefault()
 
-    //     const mmlist = document.querySelectorAll('li') 
-    //     if (title) {
-    //         let selectMM =  fetch(`api/mindmap/title`, {
-    //             method: 'POST', 
-    //             body: json.parse(title, mmlist), 
-    //             headers: {
-    //                 'content-type': "application/json"
-    //             }
-    //         })
-    //         if (selectMM.ok) {
-    //             String.prototype.replace('/dashboard')
-    //         } else {
-    //             console.error(err)
-    //             alert('error loading')
-    //         }
-    //     }
-    // }
+//     const POSTMindMap = document.querySelector('#mind-map') // click into single MM *update based on handlebars
+//     const editMindMap = document.querySelector('#editMm') // edit display on the dashboard
+//     const newMindMap = document.querySelector('#newMM') // NEW MM
+//     const deleteMindMap = document.querySelector('#deleteMM') // delete
+//     const mmlist = document.querySelectorAll('li') 
+//     const title = document.querySelector('title')
 
-    if (mmlist && title) {
 
-        const newMMChoice = await fetch(`api/mindmap/title`, {
-            method: 'POST',
-            body: JSON.stringify(mmlist, title ),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        if (newMMChoice.ok) {
-            document.location.replace('/dashboard')
-        } else {
-            alert('unable to load mindmaps')
-        }
-    }
-    if (editMindMap) {
-        const editMMChoice = await fetch(`api/mindmaps`, {
-            method: 'POST', 
-            body: JSON.stringify({ editMindMap }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        if (editMMChoice.ok) {
-            document.location.replace('/projectquestions')
-        } else {
-            alert('unable to load mindmaps')
-        }
-    }
-    if (newMindMap) {
-        const newMindMapChoice = await fetch(`api/mindmaps`, {
-            method: 'POST',
-            body: JSON.stringify({ newMindMapChoice }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
+//     // function sidebarToggler() {
+
+//     //     const mmlist = document.querySelectorAll('li') 
+//     //     if (title) {
+//     //         let selectMM =  fetch(`api/mindmap/title`, {
+//     //             method: 'POST', 
+//     //             body: json.parse(title, mmlist), 
+//     //             headers: {
+//     //                 'content-type': "application/json"
+//     //             }
+//     //         })
+//     //         if (selectMM.ok) {
+//     //             String.prototype.replace('/dashboard')
+//     //         } else {
+//     //             console.error(err)
+//     //             alert('error loading')
+//     //         }
+//     //     }
+//     // }
+
+//     if (mmlist && title) {
+
+//         const newMMChoice = await fetch(`api/mindmap/title`, {
+//             method: 'POST',
+//             body: JSON.stringify(mmlist, title ),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             }
+//         })
+//         if (newMMChoice.ok) {
+//             document.location.replace('/dashboard')
+//         } else {
+//             alert('unable to load mindmaps')
+//         }
+//     }
+//     if (editMindMap) {
+//         const editMMChoice = await fetch(`api/mindmaps`, {
+//             method: 'POST', 
+//             body: JSON.stringify({ editMindMap }),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             }
+//         })
+//         if (editMMChoice.ok) {
+//             document.location.replace('/projectquestions')
+//         } else {
+//             alert('unable to load mindmaps')
+//         }
+//     }
+//     if (newMindMap) {
+//         const newMindMapChoice = await fetch(`api/mindmaps`, {
+//             method: 'POST',
+//             body: JSON.stringify({ newMindMapChoice }),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             }
+//         })
       
-        if (newMindMapChoice.ok) {
-            document.location.replace('/projectquestions')
-        } else {
-            alert('unable to load mindmaps')
-        }
-    }
-    if (deleteMindMap) {
-        const deleteMindMapChoice = await fetch(`api/mindmaps`, {
-            method: 'POST',
-            body: JSON.stringify({ deleteMindMapChoice }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
+//         if (newMindMapChoice.ok) {
+//             document.location.replace('/projectquestions')
+//         } else {
+//             alert('unable to load mindmaps')
+//         }
+//     }
+//     if (deleteMindMap) {
+//         const deleteMindMapChoice = await fetch(`api/mindmaps`, {
+//             method: 'POST',
+//             body: JSON.stringify({ deleteMindMapChoice }),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             }
+//         })
 
-        if (deleteMindMapChoice.ok) {
-            document.location.replace('/mindmap')
-        } else {
-            alert('unable to load mindmaps')
-        }
-    }
+//         if (deleteMindMapChoice.ok) {
+//             document.location.replace('/mindmap')
+//         } else {
+//             alert('unable to load mindmaps')
+//         }
+//     }
 
-// querySelector('#sidebar').addEventListener('click', sidebarToggler)
-};
+// // querySelector('#sidebar').addEventListener('click', sidebarToggler)
+// };
 
-addEventListener('click', dashboardRo)
+// addEventListener('click', dashboardRo)
 
 
 
